cbc3654c2697b045d58962bc372e74a616f502f3
Bharat Rav <bharatrav8077@gmail.com>
2023-03-31 06:31:03 +0530

    deck class added in which we are adding the object in the arraylist of size 52 and 2 methods are defined draRandomCard and remainingCardsInDeck

--- src/deckOfCards/Deck.java
+++ src/deckOfCards/Deck.java
@@ -26,10 +26,4 @@ public class Deck {
         return result;
     }
 
-    public static void main(String[] args) {
-        Deck deck = new Deck();
-        System.out.println(deck.remainingCardsInDeck());
-        System.out.println(deck.drawRandomCard().cardDetails());
-        System.out.println(deck.remainingCardsInDeck());
-    }
 }
