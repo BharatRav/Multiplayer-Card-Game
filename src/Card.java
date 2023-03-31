a4e4b77a4e17b829dc0d45ce33f9518734444665
Bharat Rav <bharatrav8077@gmail.com>
2023-03-31 06:06:06 +0530

    Card Class added

---
+++ src/Card.java
@@ -0,0 +1,26 @@
+public class Card {
+    private int type,value;
+    boolean isActionCard;
+    private String[] cardType = {"Clubs", "Spades", "Diamonds", "Hearts"};
+    private String[] cardValue ={"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+
+    public Card(int types, int values) {
+        type =types;
+        value =values;
+
+
+        if(values<4) {
+            isActionCard =true;
+        }
+        else {
+            isActionCard =false;
+        }
+    }
+    public String cardDetails() {
+        String finalCard = cardValue[value]+" of "+cardType[type];
+        if (isActionCard) {
+            finalCard = finalCard+" [Action Card]";
+        }
+        return  finalCard;
+    }
+}
--- src/StartTheGame.java
+++ src/StartTheGame.java
@@ -1,2 +1,28 @@
 public class StartTheGame {
+
+    public StartTheGame() {
+        for (int i=0;i<60;i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        for (int i=0;i<18;i++) {
+            System.out.print("-");
+        }
+        System.out.print("WELCOME_TO_THE_CARD_GAME");
+        for (int i=0;i<18;i++) {
+            System.out.print("-");
+        }
+
+        System.out.println();
+
+        for (int i=0;i<60;i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        StartTheGame game = new StartTheGame();
+    }
 }
